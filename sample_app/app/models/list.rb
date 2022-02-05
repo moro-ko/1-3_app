78d36692362cfb6eee7fc92748f994efe193c08f
EC2 Default User <ec2-user@ip-172-31-35-134.ec2.internal>
2022-02-05 05:16:16 +0000

    [Finish]3-11

--- sample_app/app/models/list.rb
+++ sample_app/app/models/list.rb
@@ -5,5 +5,5 @@ class List < ApplicationRecord
   # バリデーションを設定：入力必須指定項目の入力有無、入力データの長さ、数値の範囲など、入力データの存在をチェック
   validates :title, presence: true
   validates :body, presence: true
-  Lvalidates :image, presence: true
+  validates :image, presence: true
 end
